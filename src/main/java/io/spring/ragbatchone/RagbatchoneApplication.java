e302a96a7ced07a58fb63ef5d63491bf54e4d4e1
Glenn Renfro <grenfro@vmware.com>
2025-02-21 14:57:28 -0500

    Introduction to repository

---
+++ src/main/java/io/spring/ragbatchone/RagbatchoneApplication.java
@@ -0,0 +1,13 @@
+package io.spring.ragbatchone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagbatchoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagbatchoneApplication.class, args);
+	}
+
+}
